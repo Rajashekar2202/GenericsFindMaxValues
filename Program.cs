45adfbedbf1454583cc3d992b53da61a270a1b98
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-24 17:08:04 +0530

    added MaxInAllGeneric program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("The Maximum Values Are \n");
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            MaxInArrayGeneric<int> generic = new MaxInArrayGeneric<int>(intArray);
+            generic.PrintMaxValue();
+            float[] floatArray = { 11.2f, 34.4f, 4.32f, 55.5f, 6.78f };
+            MaxInArrayGeneric<float> genericFloat = new MaxInArrayGeneric<float>(floatArray);
+            genericFloat.PrintMaxValue();
+            string[] stringArray = { "Red", "Blue", "Orange", "Green", "Yellow" };
+            MaxInArrayGeneric<string> genericString = new MaxInArrayGeneric<string>(stringArray);
+            genericString.PrintMaxValue();
+        }
+    }
+}
