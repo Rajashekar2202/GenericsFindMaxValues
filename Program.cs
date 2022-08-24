16d600c9e4739a2825437074aec26bfde62a59d0
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-24 17:04:05 +0530

    added MaxInString program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Value in 3 strings\n");
+            TestCode.TestCase1String();
+            TestCode.TestCase2String();
+            TestCode.TestCase3String();
+        }
+    }
+}
---
+++ TestCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class TestCode
+    {
+        public static void TestCase1String()
+        {
+            String result = MaxString.MaximumString("Pomagranet", "Apple", "Banana");
+            Console.WriteLine("Maximum value is " + result);
+        }
+        
+        public static void TestCase2String()
+        {
+            String result = MaxString.MaximumString("Lion", "Tiger", "Elephant");
+            Console.WriteLine("Maximum value is " + result);
+        }
+        
+        public static void TestCase3String()
+        {
+            String result = MaxString.MaximumString("Pink", "Green", "Yellow");
+            Console.WriteLine("Maximum value is " + result);
+        }
+
+    }
+}
