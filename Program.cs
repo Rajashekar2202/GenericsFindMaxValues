71b53ec2137956d29dd9e1524ec77d57087dcb48
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-24 17:01:57 +0530

    added MaxInFloat program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {          
+            Console.WriteLine("Find Maximum Value in 3 float numbers\n");
+            TestCode.TestCase1Float();
+            TestCode.TestCase2Float();
+            TestCode.TestCase3Float();
+        }
+    }
+}
---
+++ TestCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class TestCode
+    {
+        public static void TestCase1Float()
+        {
+            float result = MaxFloat.MaximumFloat(23.4f, 7.8f, 16.4f);
+            Console.WriteLine("Maximum value is " + result);
+        }       
+        public static void TestCase2Float()
+        {
+            float result = MaxFloat.MaximumFloat(3.4f, 17.4f, 2.5f);
+            Console.WriteLine("Maximum value is " + result);
+        }
+        public static void TestCase3Float()
+        {
+            float result = MaxFloat.MaximumFloat(3.6f, 7.5f, 21.8f);
+            Console.WriteLine("Maximum value is " + result);
+        }
+    }
+}
